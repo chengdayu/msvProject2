de88e8bf1b0a41997be88e37f7a909b226f80d94
xiaoguoke <kaiyangxidian@126.com>
2015-04-15 22:34:58 +0800

    杨凯修改函数形参输出

--- MSV/_MSVInput.m
+++ MSV/_MSVInput.m
@@ -1,30 +1,13 @@
-<<<<<<< HEAD
-float * f(float* a)
+function float* f(int x)
 {
-   return a
-};
-frame(x) and
-(
-  int x and skip;
-  x:=x+2
-=======
-frame(x,y) and 
-(
-<<<<<<< HEAD
-int x<==0 and skip;
-await(x=0);
-int y<==2 and skip
-)
-=======
- x:=x+1;
+ frame(x) and
+ (
+ x<==0 and skip;
  return x
  )
 };
 frame(x) and
 (
-int x and skip;
-f(x)
+int x and skip
 
->>>>>>> 4c3410759b45c5af26fc012576bab802c4ed1aff
 )
->>>>>>> c7006a97d3ea4c7cf74b3454279e96b7f30fe1d6
--- MSV/_MSVOutput.c
+++ MSV/_MSVOutput.c
@@ -1,6 +1,19 @@
-Line 1: parse error, expecting `$'
-1 error(s) were found.
-__Pre() error!
+#include<stdio.h>
+#include<stdlib.h>
+int $state_num=0;
+float *  f(int $Ext,float* x){
+x=0;
+printf("state %d:", $state_num);
+printf();
+printf("\n");
+$state_num++;
+return x;
+}
 int main() {
-Falsereturn 0;
+int x;
+printf("state %d:", $state_num);
+printf("x=%d ",x);
+printf("\n");
+$state_num++;
+return 0;
 }
