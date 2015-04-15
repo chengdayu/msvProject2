881eb7abae814ba0f403e3d4b67a59e285b760ba
xiaoguoke <kaiyangxidian@126.com>
2015-04-15 22:47:40 +0800

    杨凯添加函数形参输出

--- MSV/_MSVInput.m
+++ MSV/_MSVInput.m
@@ -1,13 +1,4 @@
-function float* f(int x)
-{
- frame(x) and
- (
- x<==0 and skip;
- return x
- )
-};
 frame(x) and
 (
-int x and skip
-
+  int x<==3 and skip
 )
--- MSV/_MSVOutput.c
+++ MSV/_MSVOutput.c
@@ -1,16 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-int $state_num=0;
-float *  f(int $Ext,float* x){
-x=0;
-printf("state %d:", $state_num);
-printf();
-printf("\n");
-$state_num++;
-return x;
-}
 int main() {
 int x;
+x=3;
 printf("state %d:", $state_num);
 printf("x=%d ",x);
 printf("\n");
