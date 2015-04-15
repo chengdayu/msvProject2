616a41cd9c2bba16b084e0706c5d703fefb8b905
user <642324358@qq.com>
2015-04-15 22:27:22 +0800

    2015-4-15王猛修改函数参数符号表

--- MSV/_MSVInput.m
+++ MSV/_MSVInput.m
@@ -1,13 +1,4 @@
-int f(int x)
-{frame(x) and
-(
- x:=x+1;
- return x
- )
-};
 frame(x) and
 (
-int x and skip;
-f(x)
-
+int x and skip
 )
--- MSV/_MSVOutput.c
+++ MSV/_MSVOutput.c
@@ -1,19 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 int $state_num=0;
-int f(int $Ext,int x){
-printf("state %d:", $state_num);
-printf();
-printf("\n");
-$state_num++;
-return x;x=x+1;
-}
 int main() {
 int x;
 printf("state %d:", $state_num);
 printf("x=%d ",x);
 printf("\n");
 $state_num++;
-f(1,x);
 return 0;
 }
