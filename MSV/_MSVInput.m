06ef593fdb089b0c588494db037f81d494566563
user <642324358@qq.com>
2015-04-16 11:24:42 +0800

    2015-4-16王猛同步2

--- MSV/_MSVInput.m
+++ MSV/_MSVInput.m
@@ -1,13 +1,12 @@
-function int f(int x)
-{ 
-  frame(x) and
-  (
-      x:=x+1;
-      return x
-  )
+function int f(int x,int y)
+{
+x:=x+1;
+ return x
 };
 frame(x,y) and
 (
-   int x and skip;
-   x:=f(x)
+int x and skip;
+int y and skip;
+x<== extern f(x,y) and skip
+
 )
--- MSV/_MSVOutput.c
+++ MSV/_MSVOutput.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 int $state_num=0;
-int  f(int $Ext,int x){
+int  f(int $Ext,int x,int y){
 printf("state %d: ", $state_num);
 printf("x=%d ",x);
+printf("y=%d ",y);
 printf("\n");
 $state_num++;
 x=x+1;
@@ -15,13 +16,22 @@ printf("state %d: ", $state_num);
 printf("x=%d ",x);
 printf("\n");
 $state_num++;
+int y;
 printf("state %d: ", $state_num);
 printf("x=%d ",x);
+printf("y=%d ",y);
 printf("\n");
 $state_num++;
-x=f(0,x);
+x=f(1,x,y);
+;
 printf("state %d: ", $state_num);
 printf("x=%d ",x);
+printf("y=%d ",y);
+printf("\n");
+$state_num++;
+printf("state %d: ", $state_num);
+printf("x=%d ",x);
+printf("y=%d ",y);
 printf("\n");
 $state_num++;
 return 0;
