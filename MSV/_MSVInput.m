225b369ab9857231dfde7d081b4c03b9b328b8bf
xiaoguoke <kaiyangxidian@126.com>
2015-04-16 10:40:53 +0800

    yangkai

--- MSV/_MSVInput.m
+++ MSV/_MSVInput.m
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
-function int f(int x,int y)
+function int f(int x)
 {
- return x
-};
-frame(x,y) and
-(
-int x and skip;
-int y and skip;
-x<== extern f(x,y) and skip
-
-=======
 frame(x) and
 (
-<<<<<<< HEAD
-    len(10)
-=======
-  int x<==3 and skip
->>>>>>> 82f53d4beebda1234cd7b6808e9a656c036f07f0
->>>>>>> 98edb4e7b20d055af78bbe5662fb336af29d3b89
+  x:=0;
+  return x
+  )
+};
+frame(x) and 
+(
+  int x and skip;
+  x:=f(x)
 )
