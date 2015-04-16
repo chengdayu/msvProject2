c9cd9ff150decd760c9a44daf27bae18822576a8
user <642324358@qq.com>
2015-04-16 10:06:58 +0800

    2015-4-16王猛修改函数定义参数符号表1

--- MSV/_MSVInput.m
+++ MSV/_MSVInput.m
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
-function int f(int x,int y)
-{
- return x
+function int f(int x)
+{ 
+  frame(x) and
+  (
+      x:=x+1;
+      return x
+  )
 };
 frame(x,y) and
 (
-int x and skip;
-int y and skip;
-x<== extern f(x,y) and skip
-
-=======
-frame(x) and
-(
-  int x<==3 and skip
->>>>>>> 82f53d4beebda1234cd7b6808e9a656c036f07f0
+   int x and skip;
+   x:=f(x)
 )
