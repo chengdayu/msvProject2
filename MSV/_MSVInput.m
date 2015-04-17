42bd647905db0033f5d7f95ed887bdf41d3ff799
xiaoguoke <kaiyangxidian@126.com>
2015-04-17 10:51:04 +0800

    杨凯

--- MSV/_MSVInput.m
+++ MSV/_MSVInput.m
@@ -1,14 +1,13 @@
-function int f(int f_x)
-{frame(f_x) and
+function int f(int x)
+{frame(x) and
 (
- f_x:=f_x+1;
- f_x:=f_x+1;
- return f_x
+ x:=x+1;
+ x:=x+1;
+ return x
  )
 };
 frame(x) and
 (
 int x<==0 and skip;
-x:= f(x) and skip
-
+x:= extern f(x)
 )
