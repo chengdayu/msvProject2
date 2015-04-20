9a91aed15a79b814c15da47f8a3153f073d91449
xiaoguoke <kaiyangxidian@126.com>
2015-04-20 11:39:04 +0800

    杨凯

--- MSV/_MSVInput.m
+++ MSV/_MSVInput.m
@@ -1,40 +1,8 @@
-<<<<<<< HEAD
-function float* fsz ( int n)
- {
-     frame(return_1, fp) and ( 
-     int return_1<==0 and skip;
-	// float* fp<==(float *)malloc(n*sizeof(float)) and skip;
-     return fp
-     )
- };
- frame(x) and
+
+
+ frame(x,p) and
  (
-   int x and skip
+   int x and skip;
+   int *p and skip;
+   p<==(int *)(extern malloc(4)) and skip
  )
-=======
-<<<<<<< HEAD
-function int f(int f_x)
-{
-   frame(f_x) and 
-   (
-	  return f_x
-   )
-};
-frame(x) and
-(
-  int x<==extern f(x)+4 and empty
-=======
-frame(x,y) and 
-(
-int x<==3 and int y<==0 and skip;
-if(x=1) then
-{
-  x:=2
-}
-else
-{
-  x:=3
-}
->>>>>>> c2d8e9faa831b931f2a158e360bae6652ee5fde5
-)
->>>>>>> ebb236e6f1a0db977cbc50b088c5b08c0a9639ff
