8b52e83c6b21f62bdbddf4e018e4c7981ce75ad8
user <642324358@qq.com>
2015-04-22 17:24:42 +0800

    2015-4-22王猛同步7

--- MSV/_MSVInput.m
+++ MSV/_MSVInput.m
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-char name[4]={"空手","带狼","带羊","带菜"}
-=======
-<<<<<<< HEAD
-frame(x) and
-(
-   int x<==1 and skip;
-   free(y) and skip
-)
-=======
+
 function f(int x)
 {
    frame(z) and
@@ -26,5 +17,4 @@ frame(y,z) and
 (
   int y<==3 and int z<==4 and skip
 )
->>>>>>> 7119a5846c60973a53919e31cdc99369cbc48da9
->>>>>>> 7e5305bec42a53d51d57949f0c5e2674a02f321b
+
