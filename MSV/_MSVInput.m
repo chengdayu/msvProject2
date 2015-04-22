768277cd881f25e843e037f40652816e6ea3fc36
user <642324358@qq.com>
2015-04-22 16:51:01 +0800

    2015-4-22 王猛同步5

--- MSV/_MSVInput.m
+++ MSV/_MSVInput.m
@@ -1,18 +1,1 @@
-function f(int x)
-{
-   frame(z) and
-   (
-      int z and skip
-   )
-};
-function f1(int x)
-{
-   frame(z) and
-   (
-      int z and skip
-   )
-};
-frame(y,z) and
-(
-  int y<==3 and int z<==4 and skip
-)
+char name[4]={"空手","带狼","带羊","带菜"}
