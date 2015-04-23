90b49037fba782a08548ee022c61be500667c5d6
user <642324358@qq.com>
2015-04-23 09:58:49 +0800

    2015-4-23王猛修改变量定义位置与函数声明

--- MSV/_MSVOutput.cpp
+++ MSV/_MSVOutput.cpp
@@ -1,64 +1,89 @@
-<<<<<<< HEAD
 #include<stdio.h>
 #include<stdlib.h>
 int $$state_num=0;
+void  f1(int $$Ext);
+int  f(int $$Ext);
+void  f1(int $$Ext){
+int x;
 int y;
-int z;
- f1(int $$Ext,int x){
-int z;
 if(!$$Ext){printf("state %d:", $$state_num);
-printf("y=%d ",y);
-printf("z=%d ",z);
 printf("x=%d ",x);
-printf("z=%d ",z);
+printf("\n");
+$$state_num++;
+};
+x=2;
+if(!$$Ext){printf("state %d:", $$state_num);
+printf("x=%d ",x);
+printf("\n");
+$$state_num++;
+};
+y=4;
+if(!$$Ext){printf("state %d:", $$state_num);
+printf("x=%d ",x);
+printf("y=%d ",y);
 printf("\n");
 $$state_num++;
 };
 if(!$$Ext){printf("state %d:", $$state_num);
-printf("y=%d ",y);
-printf("z=%d ",z);
 printf("x=%d ",x);
-printf("z=%d ",z);
+printf("y=%d ",y);
 printf("\n");
 $$state_num++;
 };
 }
- f(int $$Ext,int x){
-int z;
+int  f(int $$Ext){
+int  RValue;
+int x;
+int y;
 if(!$$Ext){printf("state %d:", $$state_num);
-printf("y=%d ",y);
-printf("z=%d ",z);
+printf("RValue=%d ",RValue);
 printf("x=%d ",x);
-printf("z=%d ",z);
+printf("\n");
+$$state_num++;
+};
+x=2;
+if(!$$Ext){printf("state %d:", $$state_num);
+printf("RValue=%d ",RValue);
+printf("x=%d ",x);
+printf("\n");
+$$state_num++;
+};
+y=4;
+if(!$$Ext){printf("state %d:", $$state_num);
+printf("RValue=%d ",RValue);
+printf("x=%d ",x);
+printf("y=%d ",y);
 printf("\n");
 $$state_num++;
 };
 if(!$$Ext){printf("state %d:", $$state_num);
-printf("y=%d ",y);
-printf("z=%d ",z);
+printf("RValue=%d ",RValue);
 printf("x=%d ",x);
-printf("z=%d ",z);
+printf("y=%d ",y);
 printf("\n");
 $$state_num++;
 };
+RValue=4;
+if(!$$Ext){printf("state %d:", $$state_num);
+printf("RValue=%d ",RValue);
+printf("x=%d ",x);
+printf("\n");
+$$state_num++;
+};
+return RValue;
 }
 
 int main() {
-y=3;
-z=4;
+f(0);
 printf("state %d:", $$state_num);
-printf("y=%d ",y);
-printf("z=%d ",z);
+printf("\n");
+$$state_num++;
+f1(0);
+printf("state %d:", $$state_num);
 printf("\n");
 $$state_num++;
 printf("state %d:", $$state_num);
-printf("y=%d ",y);
-printf("z=%d ",z);
 printf("\n");
 $$state_num++;
 return 0;
 }
-=======
-Line 1: parse error
-1 error(s) were found.
->>>>>>> bf832fa8d141d283108527c70bfd73010e0cb1c2
