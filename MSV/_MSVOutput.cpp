e929eea1ea819da72d43667e8c23a2b042121030
997948259 <997948259@qq.com>
2015-04-24 16:58:24 +0800

    2015/4/24于斌添加or语句

--- MSV/_MSVOutput.cpp
+++ MSV/_MSVOutput.cpp
@@ -1,2 +1,34 @@
-Line 1: parse error
-1 error(s) were found.
+#include<stdio.h>
+#include<stdlib.h>
+int $$state_num=0;
+int y;
+
+int main() {
+printf("state %d:", $$state_num);
+printf("Global: ");
+printf("y=%d ",y);
+printf("\n");
+$$state_num++;
+$$current=0;
+$$changeState();
+if ($$flag[$$current-1]==0){printf("state %d:", $$state_num);
+printf("Global: ");
+printf("y=%d ",y);
+printf("\n");
+$$state_num++;
+y=0;
+} else {printf("state %d:", $$state_num);
+printf("Global: ");
+printf("y=%d ",y);
+printf("\n");
+$$state_num++;
+y=1;
+}
+;
+printf("state %d:", $$state_num);
+printf("Global: ");
+printf("y=%d ",y);
+printf("\n");
+$$state_num++;
+return 0;
+}
